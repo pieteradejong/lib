3ce9eedd32e3dd9057a01f193e43503811a3afa0
Pieter de Jong <pieter.a.dejong@gmail.com>
2018-06-20 14:18:21 -0400

    add to personal library- basic class stub with unit tests

---
+++ python/class_with_test_template.py
@@ -0,0 +1,16 @@
+import unittest
+
+class MyClass(object):
+	def __init__(self):
+		pass
+
+class MyClassTest(unittest.TestCase):
+	def setUp(self):
+		pass
+
+	def tearDown(self):
+		pass
+
+
+if __name__ == "__main__":
+	unittest.main()
